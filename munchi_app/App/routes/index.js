08514bbdd046a82ce1e77b04b3e6dca01fddcbeb
webe0586 <webe0586@umn.edu>
2017-10-03 20:33:19 -0500

    playing with style and figuring out jade

--- munchi_app/App/routes/index.js
+++ munchi_app/App/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Colette Technologies' });
+  res.render('index', { title: 'Munchi' });
 });
 
 module.exports = router;
